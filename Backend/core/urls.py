8263a0748e2058d161d9e450216d9feb41359f63
Juancalero01 <marcelocalero5@gmail.com>
2022-03-13 23:34:11 -0300

    cambios de estilo

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from portfolio import views as vw
+from django.conf import settings
 
 urlpatterns = [
+    path('', vw.index, name="index"),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
